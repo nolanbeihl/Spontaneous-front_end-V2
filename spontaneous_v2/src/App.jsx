8316b69a6b3d7d223f08f7254fba8bda22537b4f
Nolan Beihl <nolan.beihl@gmail.com>
2022-05-06 15:26:08 -0600

    Framework for App completed

---
+++ spontaneous_v2/src/App.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import axios from 'axios';
+import { Component } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './index.css';
+
+
+class App extends Component{
+    constructor(props) {
+        super(props);
+        this.state ={
+
+        }
+    }
+    render(){
+        return(
+            <h1> This is the framework for Spontaneous Version 2!!</h1>
+
+        );
+    }
+}
+export default App;
