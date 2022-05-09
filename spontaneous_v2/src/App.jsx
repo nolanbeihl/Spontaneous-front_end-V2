c44aef93c1e28b1cbeb4e690261e27c360690bf3
Nolan Beihl <nolan.beihl@gmail.com>
2022-05-09 16:31:01 -0600

    Testing use of Yelp API with basic search parameters

--- spontaneous_v2/src/App.jsx
+++ spontaneous_v2/src/App.jsx
@@ -9,12 +9,28 @@ class App extends Component{
     constructor(props) {
         super(props);
         this.state ={
+            businessResult : [],
 
         }
     }
+    componentDidMount(){
+        this.findbusiness()
+    }
+    findbusiness = async() =>{
+        let response = await axios.get('https://api.yelp.com/v3/businesses/search?term=delis')
+        this.setState({
+            businessResult : response.data,
+        })
+        console.log(response)
+    }
     render(){
         return(
-            <h1> This is the framework for Spontaneous Version 2!!</h1>
+            <><h1> This is the framework for Spontaneous Version 2!!</h1>
+            <div className="container">
+            <h1> Test of Yelp API call</h1>
+            
+
+            </div></>
 
         );
     }
