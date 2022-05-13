7d6b678d12321cb7e82f2fcefe89aec5944b96da
Nolan Beihl <nolan.beihl@gmail.com>
2022-05-13 11:15:24 -0600

    Working on getting yelp API to work, running into issue with response due to authentication and CORS.  Will continue to troubleshoot and resolve before continuing the build

--- spontaneous_v2/src/App.jsx
+++ spontaneous_v2/src/App.jsx
@@ -10,14 +10,30 @@ class App extends Component{
         super(props);
         this.state ={
             businessResult : [],
+            GAPI: 'AIzaSyAORImFpOkztcGpu9e3iInNX_nB10Cn8AA',
+            YAPI : '8Z7GDGxlSkr6QszFGN3-MP7-nKUy49NayN6fgRXt_0_kbeiaswOBI5ADUUUjxsc1y4sfwUBw178a3x0FiChWq-bhN8aMveN33MSTVbpGkSt6QIBeDrdPUWYi-Ay5YXYx',
+            lat: +38.9072,
+            lng: -77.0369,
+
 
         }
     }
     componentDidMount(){
-        this.findbusiness()
+        this.findbusiness();
+        this.getLocation();
+    }
+
+    getLocation = async() =>{
+        let response = await axios.post(`https://www.googleapis.com/geolocation/v1/geolocate?key=${this.state.GAPI}`)
+        // this.convertLocation();
+        this.setState({
+            lat: (response.data.location.lat),
+            lng: (response.data.location.lng)
+        })
+        console.log(this.state.lat, this.state.lng)
     }
     findbusiness = async() =>{
-        let response = await axios.get('https://api.yelp.com/v3/businesses/search?term=delis')
+        let response = await axios.get(`https://api.yelp.com/v3/businesses/search?term=delis&latitude=${this.state.lat}&longitude=${this.state.lng}/` , {headers: {Authorization: 'Bearer ' + this.state.YAPI}});
         this.setState({
             businessResult : response.data,
         })
@@ -28,6 +44,8 @@ class App extends Component{
             <><h1> This is the framework for Spontaneous Version 2!!</h1>
             <div className="container">
             <h1> Test of Yelp API call</h1>
+            {/* <button onClick={this.getLocation()}>Get Location</button> */}
+            {/* <this.findbusiness/> */}
             
 
             </div></>
